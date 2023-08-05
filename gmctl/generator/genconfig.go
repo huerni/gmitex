31dda4b25d301da3db8bd15585f7c8d5de9e728e
huerni <47264950+huerni@users.noreply.github.com>
2023-08-05 15:35:57 +0800

    feat(gmctl): update configgst

--- gmctl/generator/genconfig.go
+++ gmctl/generator/genconfig.go
@@ -11,33 +11,9 @@ import (
 //go:embed tpl/server/internal/config/config.tpl
 var configTemplate string
 
-//go:embed tpl/server/internal/config/etcdconfig.tpl
-var etcdTemplate string
-
-//go:embed tpl/server/internal/config/mysqlconfig.tpl
-var mysqlTemplate string
-
-//go:embed tpl/server/internal/config/traefikconfig.tpl
-var traefikTemplate string
-
 func (g *Generator) GenConfig(ctx DirContext, proto parser.Proto) error {
 	configfileName := "config"
 	fileName := filepath.Join(ctx.GetConfig().Filename, fmt.Sprintf("%v.go", configfileName))
-
-	err := util.With("config").GoFmt(true).Parse(etcdTemplate).SaveTo(map[string]any{}, filepath.Join(ctx.GetConfig().Filename, "etcdconfig.go"), false)
-	if err != nil {
-		return err
-	}
-
-	err = util.With("config").GoFmt(true).Parse(mysqlTemplate).SaveTo(map[string]any{}, filepath.Join(ctx.GetConfig().Filename, "mysqlconfig.go"), false)
-	if err != nil {
-		return err
-	}
-
-	err = util.With("config").GoFmt(true).Parse(traefikTemplate).SaveTo(map[string]any{}, filepath.Join(ctx.GetConfig().Filename, "traefikconfig.go"), false)
-	if err != nil {
-		return err
-	}
 
 	return util.With("config").GoFmt(true).Parse(configTemplate).SaveTo(map[string]any{}, fileName, false)
 
